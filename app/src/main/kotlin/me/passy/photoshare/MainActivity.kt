f47715bc07747a9a46e50778b9b5a87961af4653
Pascal Hartig <phartig@rdrei.net>
2016-01-17 20:40:35 +0000

    Twitter login shenanigans

--- app/src/main/kotlin/me/passy/photoshare/MainActivity.kt
+++ app/src/main/kotlin/me/passy/photoshare/MainActivity.kt
@@ -3,8 +3,8 @@ package me.passy.photoshare
 import android.app.Activity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.widget.Toolbar
+import com.parse.ui.ParseLoginBuilder
 
 class MainActivity : Activity() {
 
@@ -15,6 +15,9 @@ class MainActivity : Activity() {
         setActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        fab.setOnClickListener { view ->
+            val builder = ParseLoginBuilder(this);
+            startActivityForResult(builder.build(), 0);
+        }
     }
 }
