716d673027466a11bf913bd9b528d8e45170f194
Pascal Hartig <phartig@rdrei.net>
2016-01-17 20:33:04 +0000

    Fix crashes

--- app/src/main/kotlin/me/passy/photoshare/PhotoShareApplication.kt
+++ app/src/main/kotlin/me/passy/photoshare/PhotoShareApplication.kt
@@ -1,12 +1,15 @@
 package me.passy.photoshare
 
 import android.support.multidex.MultiDexApplication
+import com.parse.Parse
 import com.parse.ParseTwitterUtils
 
 public class PhotoShareApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
+       Parse.enableLocalDatastore(this);
+        Parse.initialize(this, BuildConfig.PARSE_APPLICATION_ID, BuildConfig.PARSE_CLIENT_KEY);
         ParseTwitterUtils.initialize(BuildConfig.PARSE_APPLICATION_ID, BuildConfig.PARSE_CLIENT_KEY)
     }
 }
