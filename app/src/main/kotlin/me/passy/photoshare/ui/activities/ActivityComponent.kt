2155684f4991dc90ea5d9946f8d37faff4c5bf66
Pascal Hartig <phartig@rdrei.net>
2016-02-08 19:57:03 +0000

    Provider for my presenter

--- app/src/main/kotlin/me/passy/photoshare/ui/activities/ActivityComponent.kt
+++ app/src/main/kotlin/me/passy/photoshare/ui/activities/ActivityComponent.kt
@@ -2,9 +2,10 @@ package me.passy.photoshare.ui.activities
 
 import dagger.Subcomponent
 import me.passy.photoshare.ui.ForActivity
+import me.passy.photoshare.ui.presenters.PresenterModule
 
 @ForActivity
-@Subcomponent(modules = arrayOf(ActivityModule::class))
+@Subcomponent(modules = arrayOf(ActivityModule::class, PresenterModule::class))
 interface ActivityComponent {
     fun inject(activity: StreamActivity)
     fun inject(activity: PhotoUploadActivity)
--- app/src/main/kotlin/me/passy/photoshare/ui/presenters/PresenterModule.kt
+++ app/src/main/kotlin/me/passy/photoshare/ui/presenters/PresenterModule.kt
@@ -2,3 +2,10 @@ package me.passy.photoshare.ui.presenters
 
 import dagger.Module
 import dagger.Provides
+
+@Module
+class PresenterModule {
+    @Provides
+    fun providePhotoUploadPresenter(presenter: PhotoUploadPresenterImpl): PhotoUploadPresenter
+            = presenter
+}
